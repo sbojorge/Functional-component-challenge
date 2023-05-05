6fc1ea952cdd2f5de783522570b5c63287048231
sbojorge <>
2023-05-05 09:32:00 +0000

    style the links

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,8 +1,9 @@
 import React from "react";
+import css from "./css/Sidebar.module.css";
 
 
 function SideBar() {
-    return <div>
+    return <div className = {css.sidebar}>
         <a href = "#." target= "_blank">My photos</a>
         <a href = "#." target= "_blank">My illustrations</a>
         <a href = "#." target= "_blank">My paintings</a>
