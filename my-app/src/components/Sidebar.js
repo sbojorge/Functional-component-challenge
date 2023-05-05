3209031b8a8e7a11f8f095df579ae79e3d0dbde1
sbojorge <>
2023-05-05 09:25:41 +0000

    create SideBar functional component

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function SideBar() {
+    return <div>
+        <a href = "#." target= "_blank">My photos</a>
+        <a href = "#." target= "_blank">My illustrations</a>
+        <a href = "#." target= "_blank">My paintings</a>
+    </div>
+}
+
+export default SideBar;
