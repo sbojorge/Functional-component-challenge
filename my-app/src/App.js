57c477c7a535cf970cb07ca4e28df8f366da29b6
sbojorge <>
2023-05-08 20:30:24 +0000

    create a stateful class and add it a state

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import css from './App.module.css';
 import SideBar from './components/Sidebar';
+import NavBarSimple from './components/NavBarSimple';
 
 function App() {
   return (
     <div className={css.App}>
       <SideBar />
+      <NavBarSimple />
     </div>
   );
 }
---
+++ my-app/src/components/NavBarSimple.js
@@ -0,0 +1,40 @@
+import React from "react";
+import css from './css/NavBarSimple.module.css';
+
+class NavBarSimple extends React.Component {
+    
+    constructor(props) {
+        super(props);
+        this.state = {
+            message: "Hello, guest!",
+            buttonText: "Log in"
+        };
+    }
+       
+    handleClick() {
+        this.setState((prevState) => {
+            console.log(this.state.message);
+            return {
+                message: prevState.message === "Hello, guest!" ? "Welcome back, user!" : "Hello, guest!",
+                buttonText: prevState.buttonText === "Log in" ? "Log out" : "Log in"
+            };
+        });
+        
+    }
+
+
+    render() {
+        return (
+            <div className = {css.NavBar}>
+                <h1>My Gallery</h1>
+                <div>
+                    <span>{this.state.message}</span>
+                    <button onClick={() => this.handleClick()}>{this.state.buttonText}</button>
+                    
+                </div>
+            </div>
+        )
+    }
+}
+
+export default NavBarSimple;
