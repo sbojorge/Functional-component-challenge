f14c94efd314c27cd19930663dda01f021c17ead
sbojorge <>
2023-05-13 10:42:10 +0000

    refactor the class component

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import {savedPosts} from "../posts.json";
 import css from './css/Content.module.css';
+import PostItem from './PostItem';
 
 export class Content extends Component {
     constructor(props) {
@@ -14,20 +15,9 @@ export class Content extends Component {
             <h1>My Photos</h1>
         </div>
         <div className={css.SearchResults}>
-            {
-                savedPosts.map(post =>{
-                    return(
-                        <div key={post.title}className={css.SearchItem}>
-                            <p>{post.title}</p>
-                            <p>{post.name}</p>
-                            <img src={post.image} alt='random image' />
-                            <p>{post.description}</p>
-
-                        </div>
-                    )
-                        
-                })
-            }
+            <PostItem savedPosts={savedPosts}/>
+                
+            
         </div>
       </div>
     )
