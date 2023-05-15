420aa6c638ec17e7980d8372d1dbc29268ce6e7e
sbojorge <>
2023-05-15 14:54:08 +0000

    use component life cycle methods

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import {savedPosts} from "../posts.json";
 import css from './css/Content.module.css';
 import PostItem from './PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
     constructor(props) {
       super(props)
-      this.state = {}
+      this.state = {
+        isLoaded: false
+      }
     }
+    updateState() {
+        setTimeout(() => {
+            this.setState({
+                isLoaded: true
+            })
+        }, 2000)
+    }
+    componentDidMount() {
+        this.updateState()
+    }        
     render() {
     return (
       <div className={css.Content}>
@@ -15,7 +28,12 @@ export class Content extends Component {
             <h1>My Photos</h1>
         </div>
         <div className={css.SearchResults}>
-            <PostItem savedPosts={savedPosts}/>
+            {/* Component Lifecycle challenge:conditionally render the PostItem component
+            vs the Loader component based on the state of isLoaded */}
+            {this.state.isLoaded ? <PostItem savedPosts={savedPosts}/> : <Loader />}
+            
+            {/* <Loader />
+            <PostItem savedPosts={savedPosts}/> */}
                 
             
         </div>
---
+++ my-app/src/components/Loader.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import css from './css/Loader.module.css';
+
+
+function Loader() {
+  return (
+    <div className= {css.Loader}></div>
+  )
+}
+
+export default Loader
