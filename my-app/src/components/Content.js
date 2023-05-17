30468a90b14c577fef3d280b41d0f6e1acf7ea85
sbojorge <>
2023-05-17 09:41:00 +0000

    create a search field and filter the results

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,7 +8,8 @@ export class Content extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        isLoaded: false
+        isLoaded: false,
+        posts: []
       }
     }
     updateState() {
@@ -20,17 +21,34 @@ export class Content extends Component {
     }
     componentDidMount() {
         this.updateState()
-    }        
+        this.setState({
+            posts:savedPosts
+        })
+    }
+    handleSearch = (event) => {
+        const name = event.target.value
+        const filteredPosts = savedPosts.filter(post => {
+            return post.name.toLowerCase().includes(name)
+        })
+        this.setState({
+            posts:filteredPosts
+        })
+    }       
     render() {
     return (
       <div className={css.Content}>
         <div className={css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+                <label htmlFor='searchInput'>Search</label>
+                <input onChange={(event) =>this.handleSearch(event)} type='search' id='searchInput' placeholder='By Author' />
+                <h4>posts found:{this.state.posts.length}</h4>
+            </form>
         </div>
         <div className={css.SearchResults}>
             {/* Component Lifecycle challenge:conditionally render the PostItem component
             vs the Loader component based on the state of isLoaded */}
-            {this.state.isLoaded ? <PostItem savedPosts={savedPosts}/> : <Loader />}
+            {this.state.isLoaded ? <PostItem savedPosts={this.state.posts}/> : <Loader />}
             
             {/* <Loader />
             <PostItem savedPosts={savedPosts}/> */}
